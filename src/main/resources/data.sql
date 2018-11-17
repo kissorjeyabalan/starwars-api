1fb2e0a7242dde987720283057f35494e219f24a
Kissor Jeyabalan <git@kij.no>
2018-11-18 00:21:55 +0100

    changaed ddl to create drop

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into vehicles (creation_time, name, model, updated,) values (CURRENT_TIMESTAMP() , 'Sand Crawler' , 'Digger Crawler', CURRENT_TIMESTAMP());
-insert into vehicles (creation_time, name, model, updated,) values (CURRENT_TIMESTAMP() , 'Sail Barge' , 'Modified Luxury Sail Barge', CURRENT_TIMESTAMP());
-insert into vehicles (creation_time, name, model, updated,) values (CURRENT_TIMESTAMP() , 'TIE Bomber' , 'TIE/SA Bomber', CURRENT_TIMESTAMP());
-insert into vehicles (creation_time, name, model, updated,) values (CURRENT_TIMESTAMP() , 'Snowspeeder' , 'T-47 Airspeeder', CURRENT_TIMESTAMP());
-insert into vehicles (creation_time, name, model, updated,) values (CURRENT_TIMESTAMP() , 'AT-AT' , 'All Terrain Armored Transport', CURRENT_TIMESTAMP());
-insert into vehicles (creation_time, name, model, updated,) values (CURRENT_TIMESTAMP() , 'AT-ST' , 'All Terrain Scout Transport', CURRENT_TIMESTAMP());
