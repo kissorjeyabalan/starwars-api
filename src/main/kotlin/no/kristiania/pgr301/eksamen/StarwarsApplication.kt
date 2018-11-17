7a3eef66b8c7283fc880477da7d5261377400e7a
Kissor Jeyabalan <git@kij.no>
2018-11-18 00:28:21 +0100

    changed port

--- src/main/kotlin/no/kristiania/pgr301/eksamen/StarwarsApplication.kt
+++ src/main/kotlin/no/kristiania/pgr301/eksamen/StarwarsApplication.kt
@@ -13,7 +13,7 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
-//@EnableSwagger2
+@EnableSwagger2
 @SpringBootApplication
 class StarwarsApplication {
     @Bean
@@ -37,7 +37,7 @@ class StarwarsApplication {
     @Bean
     fun forwardToIndex() = object: WebMvcConfigurer {
         override fun addViewControllers(registry: ViewControllerRegistry) {
-            registry.addRedirectViewController("/", "/vehicles")
+            registry.addRedirectViewController("/", "/swagger-ui.html")
         }
     }
 }
