ad8a09686f09b54ecc9fa111aab00c7463ccbcad
Kissor Jeyabalan <git@kij.no>
2018-11-20 13:06:12 +0100

    enable jpa repositories

--- src/main/java/no/kristiania/pgr301/eksamen/CrudApplication.java
+++ src/main/java/no/kristiania/pgr301/eksamen/CrudApplication.java
@@ -1,7 +1,9 @@
 package no.kristiania.pgr301.eksamen;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 /*import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -22,6 +24,7 @@ import java.net.URISyntaxException;*/
 
 //@EnableSwagger2
 @SpringBootApplication
+@EnableJpaRepositories
 public class CrudApplication {
 
     /*@Bean
