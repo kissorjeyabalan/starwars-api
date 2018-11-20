d784eb18c4ebb1942c0848628ff8952d978b074c
Kissor Jeyabalan <git@kij.no>
2018-11-20 13:18:22 +0100

    added log

--- src/main/java/no/kristiania/pgr301/eksamen/GraphiteMetricsConfig.java
+++ src/main/java/no/kristiania/pgr301/eksamen/GraphiteMetricsConfig.java
@@ -20,7 +20,9 @@ public class GraphiteMetricsConfig {
     @Bean
     public GraphiteReporter getReporter(MetricRegistry registry) {
         String host = System.getenv("GRAPHITE_HOST");
+        System.out.println("HOST IS " + host);
         String apiKey = System.getenv("HOSTEDGRAPHITE_KEY");
+        System.out.println("API KEY IS " + apiKey);
 
         if (host == null || apiKey == null) {
             return null;
