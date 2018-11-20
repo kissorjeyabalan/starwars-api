8a8b7e33e027fc51fb8cd7deb44be39759df1831
Kissor Jeyabalan <git@kij.no>
2018-11-20 13:30:45 +0100

    enable entity scan on specific package

--- src/main/java/no/kristiania/pgr301/eksamen/CrudApplication.java
+++ src/main/java/no/kristiania/pgr301/eksamen/CrudApplication.java
@@ -3,6 +3,7 @@ package no.kristiania.pgr301.eksamen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 /*import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +26,7 @@ import java.net.URISyntaxException;*/
 //@EnableSwagger2
 @SpringBootApplication
 @EnableJpaRepositories
+@EntityScan(basePackages = "no.kristiania.pgr301.eksamen.entity")
 public class CrudApplication {
 
     /*@Bean
